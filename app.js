6f2a29ce729df141501a3f369a213306e82c965e
Yll Gajtani <123652630+Yllgajtani@users.noreply.github.com>
2025-02-13 18:46:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+var cors = require('cors);
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 const swaggerOptions = {
